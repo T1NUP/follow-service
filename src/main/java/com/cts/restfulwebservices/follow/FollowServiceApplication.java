c3c67e395dd25263ab01e07cdd342d6ca12b3010
Punit Kumar Mishra <punitkm1899@gmail.com>
2022-07-25 23:38:04 +0530

    spring cloud integration

--- src/main/java/com/cts/restfulwebservices/follow/FollowServiceApplication.java
+++ src/main/java/com/cts/restfulwebservices/follow/FollowServiceApplication.java
@@ -3,12 +3,22 @@ package com.cts.restfulwebservices.follow;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class FollowServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FollowServiceApplication.class, args);
+	}
+	
+	@Bean
+	@LoadBalanced
+	public RestTemplate restTemplate()
+	{
+		return new RestTemplate();
 	}
 
 }
--- src/main/java/com/cts/restfulwebservices/follow/controller/FollowServiceController.java
+++ src/main/java/com/cts/restfulwebservices/follow/controller/FollowServiceController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -14,6 +15,7 @@ import com.cts.restfulwebservices.follow.repository.FollowRepository;
 import com.cts.restfulwebservices.follow.service.FollowService;
 
 @RestController
+@CrossOrigin
 public class FollowServiceController {
 
 	@Autowired
